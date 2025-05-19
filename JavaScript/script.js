693dd3801f9f8449bb851bad52114f0f97145208
k1383 <kantin1383@gmail.com>
2025-05-19 12:24:05 +0200

    delete

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,13 +1,23 @@
 //* Display of numbers and signs
-
 const display = document.querySelector('#display');
 
 document.querySelectorAll('button').forEach(button => {
     button.addEventListener('click',  () => {
-        display.textContent += button.textContent;
+        display.textContent += button.textContent;  //* += Because otherwise only one digit appears
     });
 });
 
-//* The Equal button
 
+//* The buttons (Division, Multiplication, Subtraction, Addition, Equal, Delete)
+const Division = document.querySelector('#Division')
+const Multiplication = document.querySelector('#Multiplication')
+const Soustraction = document.querySelector('#Soustraction')
+const Addition = document.querySelector('#Addition')
 const Egal = document.querySelector('#Egal')
+const Delete = document.querySelector('#Delete')
+
+
+//* When you click on Delete, the elements are deleted
+Delete.addEventListener('click', () => {
+    display.textContent = '';
+})
